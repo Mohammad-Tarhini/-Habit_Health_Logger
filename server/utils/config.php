2b061b1a6818bd2ba4e25d381fa5599fd9919c9b
mohamad baker tarhini <tarhinimohammadbaker@gmail.com>
2025-11-17 12:16:45 +0200

    push

--- server/utils/config.php
+++ server/utils/config.php
@@ -1,5 +1,5 @@
 <?php
 define("MAX_Tokens" , 300);
 define("GPT_VER" , "gpt-3.5-turbo");
-define("OPEN_AI_KEY" , "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");
+//define("OPEN_AI_KEY" , "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");
 ?>
